35669fef148459d3fb61ac7453363602555c43ff
mike.li <mike.li@odd-e.com>
2019-05-27 14:52:06 +1000

    Refactor the code to use By to locate the element.

--- src/test/java/bdd/odde/com/SearchPage.java
+++ src/test/java/bdd/odde/com/SearchPage.java
@@ -1,19 +1,44 @@
 package bdd.odde.com;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.util.concurrent.TimeUnit;
 
 public class SearchPage {
     private WebDriver _driver;
+    private Wait<WebDriver> _wait;
+
+    private By queryInput = By.name("q");
+    private By searchButton = By.name("btnK");
 
     public SearchPage() {
         initDriver();
-    }
+
+        _wait = new FluentWait<WebDriver>(_driver)
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(5, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+   }
 
     public void inputAndSubmit(String keyword) {
-        _driver.findElement(By.name("q")).sendKeys(keyword);
-        _driver.findElement(By.name("btnK")).submit();
+        try {
+            WebElement q = _wait.until(driver -> driver.findElement(queryInput));
+            q.sendKeys(keyword);
+
+            WebElement btnK = _wait.until(driver -> driver.findElement(this.searchButton));
+            btnK.submit();
+        } catch (NullPointerException exp) {
+            System.console().printf("Error: %s", exp.getMessage());
+        }
+
     }
 
     private WebDriver initDriver() {
@@ -24,6 +49,7 @@ public class SearchPage {
 
     public void load() {
         _driver.get("https://www.google.com");
+
     }
 
     public String getTitle() {
